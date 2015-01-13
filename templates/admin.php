75cbeab51bef82df761525fcc8eaa751fcefc691
Colin Urban <colin.urban@gmail.com>
2015-01-12 22:03:26 -0500

    minor updates

--- templates/admin.php
+++ templates/admin.php
@@ -1,11 +1,17 @@
 <?php include('includes/header.php');?>		
 	<body>
-	<div class="main-nav">
-	<a href="/">Home</a>&nbsp|&nbsp<a href="/new">New</a>
-	</div>
 	<div class="container">
+	<div class="row">
+		<div class="post col-md-12">
+			<div class="btn-group" role="group">
+				<a class="btn btn-default" href="/">Home</a>
+				<a class="btn btn-default" href="/new">New</a>
+			</div>
+		</div>
+	
+	</div>
 	<?php
-		foreach($posts as $post):
+		foreach($posts as $post):;
 			?>
 			<div class="post col-md-8">
 				<div style="float:right;" class="btn-group" role="group">
@@ -16,11 +22,10 @@
 					<?=$post->date;?>
 				</div>
 				<h1>
-					<a href="/single/"<?=$post->id;?>"><?=$post->title; ?></a>
+					<a href="/single/<?=$post->id;?>"><?=$post->title;?></a>
 				</h1>
-				<?= $post->content;?>
+				<?=$post->content;?>
 				</div>
 		<?php endforeach; ?>
-	?>
 	 </div>
 <?php include('includes/footer.php');?>
--- templates/single.php
+++ templates/single.php
@@ -10,7 +10,7 @@
 	</div>
 		<div class="row">
 			<?php
-				echo "<div class=\"posts col-md-12\">";
+				echo "<div class=\"posts col-md-8\">";
 				echo "<h1><a href=\"/single/".$post->id."\">".$post->title."</a></h1><hr style=\"position:relative; top:-20px;\"/>";
 				echo $post->content;
 				echo "<div style=\"color:#ccc;font-size:14px;display:inline;\" class=\"date\">posted".$post->date."</div></div>";
